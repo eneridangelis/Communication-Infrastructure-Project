b73df2192fb6d7ce986a55c53fdfc606255e88d9
lmm3 <lmm3@cin.ufpe.br>
2019-12-07 07:56:45 -0300

    added TCP

--- UDPClient.py
+++ UDPClient.py
@@ -3,7 +3,7 @@ from socket import*
 serverName = '172.22.70.249'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-message = input('Input lowercase sentence:')
+message = raw_input('Input lowercase sentence:')
 clientSocket.sendto(message.encode(), (serverName, serverPort))
 modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
 print(modifiedMessage.decode())
