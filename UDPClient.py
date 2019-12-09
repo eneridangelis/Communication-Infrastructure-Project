884533eb582f632b62aaef0004f6978ef2b0e586
lmm3 <lmm3@cin.ufpe.br>
2019-12-09 15:47:17 -0300

    rdt 3.0

--- UDPClient.py
+++ UDPClient.py
@@ -1,10 +1,77 @@
 from socket import*
+import pickle
 
-serverName = '172.22.68.47'
+sendSeq = 0
+sendACK = 0
+expectedSeq = 0
+expectedACK = 0
+
+def rdtSend(serverSocket, clientAddress, message):
+    global sendSeq
+    global sendACK
+    packet = (sendSeq, sendACK, message)   
+    data = pickle.dumps(packet)
+    serverSocket.sendto(data, clientAddress)
+    while True:
+        try:
+            recvData = serverSocket.recvfrom(1024)
+        except:
+            print('temporizador estourou')
+            serverSocket.sendto(data, clientAddress)
+        else:
+            recvSeq, recvACK, recvMessage = pickle.loads(recvData[0])
+            if recvACK == sendACK:
+                if sendACK == 0:
+                    sendACK = 1
+                    sendSeq = 1
+                else:
+                    sendACK = 0
+                    sendSeq = 0
+                return 
+
+def rdtRecv(serverSocket):
+    global expectedACK
+    while True:
+        try: 
+            recvData = serverSocket.recvfrom(1024)
+        except:
+            print('temporizador estourou')
+        else:
+            recvSeq, recvACK, recvMessage = pickle.loads(recvData[0])
+            if recvSeq == expectedACK:
+                packet = (expectedSeq, expectedACK, '')
+                data = pickle.dumps(packet)
+                serverSocket.sendto(data, recvData[1])
+                if expectedACK == 0:
+                    expectedACK = 1
+                else:
+                    expectedACK = 0
+                return (recvMessage, recvData[1])
+            else:
+                if expectedACK == 0:
+                    packet = (expectedSeq, 1, '')
+                    data = pickle.dumps(packet)
+                    serverSocket.sendto(data, recvData[1])
+                else:
+                    packet = (expectedSeq, 0, '')
+                    data = pickle.dumps(packet)
+                    serverSocket.sendto(data, recvData[1])
+
+serverName = ''
+serverHost = ''
 serverPort = 12000
+
+dnsPort = 15000
+dnsHost = '127.0.0.1'
+
+dnsSocket = socket(AF_INET, SOCK_DGRAM)
+serverName = raw_input('Insira o nome do servidor que vc quer se conectar: ')
+dnsSocket.sendto(serverName, (dnsHost, dnsPort))
+query = dnsSocket.recvfrom(1024)[0]
+serverHost = query.split()[1]
+dnsSocket.close()
+
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-message = raw_input('Input lowercase sentence:')
-clientSocket.sendto(message.encode(), (serverName, serverPort))
-modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-print(modifiedMessage.decode())
-clientSocket.close()
+message = 'Hello Server!'
+rdtSend(clientSocket, (serverHost, serverPort), message)
+rdtSend(clientSocket, (serverHost, serverPort), message)
--- UDPServer.py
+++ UDPServer.py
@@ -1,11 +1,83 @@
 from socket import *
+import pickle 
 
+sendSeq = 0
+sendACK = 0
+expectedSeq = 0
+expectedACK = 0
+
+def rdtSend(serverSocket, clientAddress, message):
+    global sendSeq
+    global sendACK
+    packet = (sendSeq, sendACK, message)   
+    data = pickle.dumps(packet)
+    serverSocket.sendto(data, clientAddress)
+    while True:
+        try:
+            recvData = serverSocket.recvfrom(1024)
+        except:
+            print('temporizador estourou')
+            serverSocket.sendto(data, clientAddress)
+        else:
+            recvSeq, recvACK, recvMessage = pickle.loads(recvData[0])
+            if recvACK == sendACK:
+                if sendACK == 0:
+                    sendACK = 1
+                    sendSeq = 1
+                else:
+                    sendACK = 0
+                    sendSeq = 0
+                return 
+
+def rdtRecv(serverSocket):
+    global expectedACK
+    while True:
+        try: 
+            recvData = serverSocket.recvfrom(1024)
+        except:
+            print('temporizador estourou')
+        else:
+            recvSeq, recvACK, recvMessage = pickle.loads(recvData[0])
+            if recvSeq == expectedACK:
+                packet = (expectedSeq, expectedACK, '')
+                data = pickle.dumps(packet)
+                serverSocket.sendto(data, recvData[1])
+                if expectedACK == 0:
+                    expectedACK = 1
+                else:
+                    expectedACK = 0
+                return (recvMessage, recvData[1])
+            else:
+                if expectedACK == 0:
+                    packet = (expectedSeq, 1, '')
+                    data = pickle.dumps(packet)
+                    serverSocket.sendto(data, recvData[1])
+                else:
+                    packet = (expectedSeq, 0, '')
+                    data = pickle.dumps(packet)
+                    serverSocket.sendto(data, recvData[1])
+
+dnsPort = 15000
+dnsHost = '127.0.0.1'
+
+serverAlias = 'luneri.com'
 serverPort = 12000
+serverHost = '127.0.0.1'
+
+dnsSocket = socket(AF_INET, SOCK_DGRAM)
+message = serverAlias + ' ' + serverHost
+dnsSocket.sendto(message, (dnsHost, dnsPort))
+recvMessage = dnsSocket.recvfrom(1024)[0]
+print(recvMessage)
+dnsSocket.close()
+
+
 serverSocket = socket(AF_INET, SOCK_DGRAM)
-serverSocket.bind(('', serverPort))
+serverSocket.settimeout(2.0)
+serverSocket.bind((serverHost, serverPort))
 print("The server is ready to recieve")
-while 1:
-    message, clientAddress = serverSocket.recvfrom(2048)
-    print(message)
-    modifiedMessage = message.decode().upper()
-    serverSocket.sendto(modifiedMessage.encode(), clientAddress)
+message, clientAddress = rdtRecv(serverSocket)
+print(message)
+message, clientAddress = rdtRecv(serverSocket)
+print(message)
+    
